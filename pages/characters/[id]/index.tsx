3edad772e1d781110ed21b54cdd9cb3ec398765b
FedoRich-P <panchev_fedor@mail.ru>
2025-01-11 12:54:21 +0300

    character

--- pages/characters/[id]/index.tsx
+++ pages/characters/[id]/index.tsx
@@ -1,18 +1,41 @@
 import {PageWrapper} from "../../../components/PageWrapper/PageWrapper";
-import {Header} from "../../../components/Header/Header";
+import {API} from "../../../assets/api/api";
+import {CharacterCard} from "../../../components/Card/CharacterCard/CharacterCard";
+import {getLayout} from "../../../components/Layout/BaseLayout/BaseLayout";
+import {CharacterType} from "../../../assets/api/rick-and-morty-api";
+import {GetStaticPaths, GetStaticProps} from "next";
 
+export const getStaticPaths: GetStaticPaths = async () => {
+    const {results} = await API.rickAndMorty.getCharacters()
 
-const Character = () => {
+    const paths = results.map(({id}) => ({params: {id: id.toString()}}))
 
+    return {paths, fallback: false}
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    const {id} = params || {};
+    const character = await API.rickAndMorty.getCharacter(id as string);
+    if (!character) return {notFound: true};
+    return {
+        props: {
+            character
+        }
+    }
+}
+
+const Character = ({character}: CharacterProps) => {
 
     return (
         <PageWrapper>
-            <Header/>
-            <h2>
-                dsfsdfsd
-            </h2>
+            <CharacterCard character={character}/>
         </PageWrapper>
     );
 };
 
+Character.getLayout = getLayout;
 export default Character;
+
+type CharacterProps = {
+    character: CharacterType
+}
